dd07cdcc56560eb82eb90aa43aa6e5a883d5c357
MohamedRaslan <mohamedraslang@gmail.com>
2023-06-23 04:39:09 +0300

    Chrome tutorial - Typescript - Tabs manager extension

---
+++ tabs-manager-ts/webpack.config.js
@@ -0,0 +1,77 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+const config = {
+  entry: {
+    popup: path.resolve(__dirname, "src", "popup.ts"),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+
+    new ESLintPlugin({
+      extensions: ['js', 'ts'],
+      overrideConfigFile: path.resolve(__dirname, '.eslintrc'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: ".", to: ".", context: "static" }]
+    })
+
+  ],
+  module: {
+    rules: [
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: [stylesHandler, "css-loader", 'sass-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+    config.devtool = "source-map";
+  } else {
+    config.mode = "development";
+    config.devtool = "inline-source-map";
+  }
+  return config;
+};
